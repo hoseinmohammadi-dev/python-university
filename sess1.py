a1e12cf2e0c16f9238ba1c73c1e56d5c1de0024c
hosein mohammadi <hosein011dev@gmail.com>
2025-11-17 21:54:05 +0330

    session 1

---
+++ sess1.py
@@ -0,0 +1,29 @@
+#جلسه یک تعریف متغیر و if و else
+a = 10
+b = 17
+if a > b :
+    print("yes")
+else:
+    print("no")
+    
+    
+    
+    
+#ادغام دو متغیر
+a = 2.5
+b = 10 
+a = b
+print(a , type(a))
+
+
+
+
+#انواع تایپ ها 
+a = 12
+b = 12.5 
+c = "12"
+d = "12.5"
+print("a type is : ", type(a))
+print("b type is : ", type(b))
+print("c type is : ", type(c))
+print("d type is : ", type(d))
